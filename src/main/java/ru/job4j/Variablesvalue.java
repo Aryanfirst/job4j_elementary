b5b37b7e3843d228ea9464f4be73a782a5e6df95
ariyanfirst <anakibulla@gmail.com>
2022-08-19 23:43:55 +0300

    Переменные. [#172635 #334332]

---
+++ src/main/java/ru/job4j/Variablesvalue.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Variablesvalue {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println(age);
+        char gender = 'M';
+        System.out.println(gender);
+        double height = 1.86;
+        System.out.println(height);
+        short amount = 1235;
+        System.out.println(amount);
+        int bacterium = 10000000;
+        System.out.println(bacterium);
+        long distance = 15000000;
+        System.out.println(distance);
+        boolean permission = true;
+        System.out.println(permission);
+        byte file = 8;
+        System.out.println(file);
+    }
+}
