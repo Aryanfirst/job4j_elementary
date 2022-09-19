015404b4489171efd4c5c0761c109a44fa73982c
aryanfirst <anakibulla@gmail.com>
2022-09-19 07:35:36 +0300

    Глупый бот [#31323 #337066]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,6 +9,16 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int reason = start; reason <= finish; reason++) {
+            if (reason % 2 == 0) {
+                sum = sum + reason;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
@@ -17,5 +27,8 @@ public class Counter {
         System.out.println(sum(4, 17));
         System.out.println(sum(-8, 25));
 
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
